4c84e326a449dce10894c7c7047d4ee43795b684
shitakemura <shitakemura@gmail.com>
2018-07-05 08:20:51 +0900

    update AddToughtActivity layout

---
+++ app/src/main/java/com/example/shitakemura/androidfirestore/AddToughtActivity.kt
@@ -0,0 +1,29 @@
+package com.example.shitakemura.androidfirestore
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class AddToughtActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_tought)
+    }
+
+    fun addPostClicked(view: View) {
+
+    }
+
+    fun addFunnyClicked(view: View) {
+
+    }
+
+    fun addSeriousClicked(view: View) {
+
+    }
+
+    fun addCrazyClicked(view: View) {
+
+    }
+}
