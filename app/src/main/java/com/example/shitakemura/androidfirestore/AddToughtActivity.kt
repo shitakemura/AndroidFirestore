b4e528100b84b42ba5397dc5ac6dad7bd3f2d71f
shitakemura <shitakemura@gmail.com>
2018-07-06 00:06:13 +0900

    add first firestore entry

--- app/src/main/java/com/example/shitakemura/androidfirestore/AddToughtActivity.kt
+++ app/src/main/java/com/example/shitakemura/androidfirestore/AddToughtActivity.kt
@@ -2,9 +2,15 @@ package com.example.shitakemura.androidfirestore
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_tought.*
 
 class AddToughtActivity : AppCompatActivity() {
+
+    var selectedCategory = FUNNY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,18 +18,45 @@ class AddToughtActivity : AppCompatActivity() {
     }
 
     fun addPostClicked(view: View) {
+        // add post to Firestore
+        val data = HashMap<String, Any>()
+        data.put("category", selectedCategory)
+        data.put("numComments", 0)
+        data.put("numLikes", 0)
+        data.put("thoughtText", addToughtText.text.toString())
+        data.put("timestamp", FieldValue.serverTimestamp())
+        data.put("username", addUsernameText.text.toString())
+
+        FirebaseFirestore.getInstance().collection(THOUGHT_REF)
+                .add(data)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener { exception ->  
+                    Log.e("Exception", "Could not add post: $exception")
+                }
 
     }
 
-    fun addFunnyClicked(view: View) {
 
+    fun addFunnyClicked(view: View) {
+        selectedCategory = FUNNY
+        addFunnyButton.isChecked = true
+        addSeriousButton.isChecked = false
+        addCrazyButton.isChecked = false
     }
 
     fun addSeriousClicked(view: View) {
-
+        selectedCategory = SERIOUS
+        addFunnyButton.isChecked = false
+        addSeriousButton.isChecked = true
+        addCrazyButton.isChecked = false
     }
 
     fun addCrazyClicked(view: View) {
-
+        selectedCategory = CRAZY
+        addFunnyButton.isChecked = false
+        addSeriousButton.isChecked = false
+        addCrazyButton.isChecked = true
     }
 }
---
+++ app/src/main/java/com/example/shitakemura/androidfirestore/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.shitakemura.androidfirestore
+
+// Collection Reference
+const val THOUGHT_REF = "thoughts"
+
+// Category Constants
+const val FUNNY = "funny"
+const val SERIOUS = "serious"
+const val CRAZY = "crazy"
