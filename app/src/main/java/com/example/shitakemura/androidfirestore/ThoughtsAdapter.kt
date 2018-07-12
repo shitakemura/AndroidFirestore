2755c7ed8ae67588e469283f1b6d40024b1c7129
shitakemura <shitakemura@gmail.com>
2018-07-13 00:03:16 +0900

    add adding likes

--- app/src/main/java/com/example/shitakemura/androidfirestore/ThoughtsAdapter.kt
+++ app/src/main/java/com/example/shitakemura/androidfirestore/ThoughtsAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -37,6 +38,11 @@ class ThoughtsAdapter(val thoughts: ArrayList<Thought>): RecyclerView.Adapter<Th
             val dateFormatter = SimpleDateFormat("MMM d, h:mm a", Locale.getDefault())
             val dateString = dateFormatter.format(thought.timestamp)
             timestamp?.text = dateString
+
+            likesImage?.setOnClickListener {
+                FirebaseFirestore.getInstance().collection(THOUGHT_REF).document(thought.documentId)
+                        .update(NUM_LIKES, thought.numLikes + 1)
+            }
         }
     }
 }
