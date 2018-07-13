3d32b722dc6f5ea57d8b05e8e7b759bfd08d8684
shitakemura <shitakemura@gmail.com>
2018-07-14 00:25:03 +0900

    add Login UI

---
+++ app/src/main/java/com/example/shitakemura/androidfirestore/CreateUserActivity.kt
@@ -0,0 +1,21 @@
+package com.example.shitakemura.androidfirestore
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createCreateClicked(view: View) {
+
+    }
+
+    fun createCancelClicked(view: View) {
+        
+    }
+}
---
+++ app/src/main/java/com/example/shitakemura/androidfirestore/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.shitakemura.androidfirestore
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginClicked(view: View) {
+
+    }
+
+    fun loginCreateClicked(view: View) {
+
+    }
+}
