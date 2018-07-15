d16c3304ed7f602ceca881ee40bf70b27d280ea1
shitakemura <shitakemura@gmail.com>
2018-07-16 00:31:44 +0900

    add login

--- app/src/main/java/com/example/shitakemura/androidfirestore/LoginActivity.kt
+++ app/src/main/java/com/example/shitakemura/androidfirestore/LoginActivity.kt
@@ -3,21 +3,40 @@ package com.example.shitakemura.androidfirestore
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        loginButton.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
 
+        loginButton.setOnClickListener {
+            login()
         }
 
         loginCreateButton.setOnClickListener {
             val createIntent = Intent(this, CreateUserActivity::class.java)
             startActivity(createIntent)
         }
+    }
+
+    fun login() {
+        val email = loginEmailText.text.toString()
+        val password = loginPasswordText.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener {exception ->
+                    Log.e("Exception", "Could not sign in user - ${exception.localizedMessage}")
+                }
     }
 }
